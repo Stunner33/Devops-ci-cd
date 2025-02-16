1cc0c6f1735addd4c4cf2bc56073e8adbfae6706
Stunner33 <stunnercadrick@gmail.com>
2025-02-17 01:21:22 +0530

    Added Dcokerfiles

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import { prismaClient } from "db";
+
+export default async function Home() {
+  const users = await prismaClient.user.findMany();
+  return <div>{JSON.stringify(users)}</div>;
+}
+// export const revalidate = 60 // revalidate every 60 seconds
+// or
+export const dynamic = 'force-dynamic'
